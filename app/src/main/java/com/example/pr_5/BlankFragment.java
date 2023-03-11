4608872282cec232faab5cb44e00e1095c46d1e9
Andrey <mr.dallas007@gmail.com>
2023-03-11 23:13:51 +0300

    Initialize 3 fragments

---
+++ app/src/main/java/com/example/pr_5/BlankFragment.java
@@ -0,0 +1,52 @@
+package com.example.pr_5;
+
+import static android.content.ContentValues.TAG;
+
+import android.content.Context;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentResultListener;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class BlankFragment extends Fragment {
+
+    private Context context;
+    private final int duration = Toast.LENGTH_SHORT;
+    private static final String TAG = "BlankFragment";
+    public BlankFragment() {
+        super(R.layout.fragment_blank);
+    }
+
+    ////
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        Button button = view.findViewById(R.id.button);
+        button.setOnClickListener(v -> getParentFragmentManager().beginTransaction().replace(R.id.fragment_container_view, BlankFragment2.class, null).commit());
+
+        TextView text1 = view.findViewById(R.id.wc1);
+        text1.setText("Добро пожаловать в GloLib!");
+        TextView text2 = view.findViewById(R.id.wc2);
+        text2.setText("Похоже, что у вас пока нету читательского билета!");
+        TextView text3 = view.findViewById(R.id.wc3);
+        text3.setText("Нажмите клавишу 'Начать', чтобы перейти на этап регистрации билета.");
+
+        ImageView image = view.findViewById(R.id.bookImage1);
+        image.setImageDrawable(getResources().getDrawable(R.drawable.book_svgrepo_com));
+
+    }
+}
---
+++ app/src/main/java/com/example/pr_5/BlankFragment2.java
@@ -0,0 +1,45 @@
+package com.example.pr_5;
+
+import android.content.Context;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Random;
+
+public class BlankFragment2 extends Fragment {
+    private Context context;
+    private final int duration = Toast.LENGTH_SHORT;
+
+    private static final String TAG = "BlankFragment2";
+    public BlankFragment2() {
+        super(R.layout.fragment_blank2);
+    }
+
+
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        Random r = new Random();
+        int randomNumber = (int)(Math.random()*100000);
+        TextView ticket = view.findViewById(R.id.ticket);
+        ticket.setText(String.valueOf(randomNumber));
+
+
+        Button button2 = view.findViewById(R.id.buttonToList2);
+        button2.setOnClickListener(v -> {
+            Bundle result = new Bundle();
+            result.putString("bundleKey", String.valueOf(randomNumber));
+            getParentFragmentManager().setFragmentResult("requestKey", result);
+            getParentFragmentManager().beginTransaction().replace(R.id.fragment_container_view,
+                            BlankFragment3.class, null)
+                    .commit();
+        });
+    }
+}
